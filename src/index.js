d2c538217ca922f70032df04f7ab36d9ebca6f46
Diogo Santos <diogo.magalhaescalvo@gmail.com>
2023-03-08 20:37:05 +0000

    First commit

--- src/index.js
+++ src/index.js
@@ -4,17 +4,35 @@ function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
   //... your code goes here
+  const price = product.querySelector('.price span'); //.innerText;
+  const quantity = product.querySelector('.quantity input'); //.value;
+
+  const priceValue = price.innerText;
+  const quantityValue = quantity.value;
+
+  const subtotalValue = priceValue * quantityValue;
+
+  const subtotal = product.querySelector('.subtotal span');
+  subtotal.innerText = subtotalValue;
+  return subtotalValue;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
-
   // ITERATION 2
-  //... your code goes here
+  const products = document.getElementsByClassName('product');
+  const productsArr = [...products]; //converting to an array
+
+  const subTotalValuesArray = productsArr.map(product =>
+    updateSubtotal(product)
+  );
+
+  const totalValue = subTotalValuesArray.reduce((acc, curr) => acc + curr, 0);
+  document.querySelector('#total-value span').innerText = totalValue;
 
   // ITERATION 3
   //... your code goes here
